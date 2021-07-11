074e8fcd116f2ce9355f2e358eee8e27030626e8
yufan <yufan.fong@gmail.com>
2021-07-11 19:08:58 +0800

    Update talker.cpp

--- ros_essentials/src/topic01_basics/talker_listener/talker.cpp
+++ ros_essentials/src/topic01_basics/talker_listener/talker.cpp
@@ -1,10 +1,3 @@
-/*
- * Author: Anis Koubaa for Gaitech EDU
- * Year: 2016
- *
- */
-
-
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sstream>
@@ -14,38 +7,41 @@ int main(int argc, char **argv)
 	// Initiate new ROS node named "talker"
 	ros::init(argc, argv, "talker_node");
 
-	//create a node handle: it is reference assigned to a new node
-	ros::NodeHandle n;
-	//create a publisher with a topic "chatter" that will send a String message
-	ros::Publisher chatter_publisher = n.advertise<std_msgs::String>("chatter", 1000);
-	//Rate is a class the is used to define frequency for a loop. Here we send a message each two seconds.
-	ros::Rate loop_rate(0.5); //1 message per second
+	// Create a node handle: it is reference assigned to a new node
+	ros::NodeHandle node;
+
+	// Create a publisher with a topic "chatter" that will send a String message
+	ros::Publisher chatter_publisher = node.advertise<std_msgs::String>("chatter", 1000);
+
+	// Rate is a class to define frequency for a loop. Here is 1 message every 2s.
+	ros::Rate loop_rate(0.5);
 
    int count = 0;
-   while (ros::ok()) // Keep spinning loop until user presses Ctrl+C
-   {
-       //create a new String ROS message.
-	   //Message definition in this link http://docs.ros.org/api/std_msgs/html/msg/String.html
-	   std_msgs::String msg;
+   while (ros::ok()) { // Keep spinning loop until user presses Ctrl+C
+   
+        // Create a new String ROS message.
+	    // Message definition in this link http://docs.ros.org/api/std_msgs/html/msg/String.html
+	    std_msgs::String msg;
 
-       //create a string for the data
-	   std::stringstream ss;
-	   ss << "Hello World " << count;
-	   //assign the string data to ROS message data field
-       msg.data = ss.str();
+        // Create a string for the data
+	    std::stringstream ss;
+	    ss << "Hello World " << count;
+	    // Assign the string data to ROS message data field
+        msg.data = ss.str();
 
-       //print the content of the message in the terminal
-       ROS_INFO("[Talker] YF published %s\n", msg.data.c_str());
+        // Print message content in the terminal
+        ROS_INFO("[Talker] I published %s\n", msg.data.c_str());
 
-       //Publish the message
-       chatter_publisher.publish(msg);
+        // Publish message
+        chatter_publisher.publish(msg);
 
-       ros::spinOnce(); // Need to call this function often to allow ROS to process incoming messages
+        // Call this function often for ROS to process incoming messages
+        ros::spinOnce(); 
 
-      loop_rate.sleep(); // Sleep for the rest of the cycle, to enforce the loop rate
-       count++;
+        // Sleep for the rest of the cycle, to enforce the loop rate
+        loop_rate.sleep(); 
+
+        count++;
    }
    return 0;
 }
-
-
