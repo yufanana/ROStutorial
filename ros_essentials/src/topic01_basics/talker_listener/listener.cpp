14982ebd6446461f4eac2b1e37af0e745b6ebc6f
yufan <yufan.fong@gmail.com>
2021-07-11 19:08:17 +0800

    Update listener.cpp

--- ros_essentials/src/topic01_basics/talker_listener/listener.cpp
+++ ros_essentials/src/topic01_basics/talker_listener/listener.cpp
@@ -1,9 +1,3 @@
-/*
- * Author: Anis Koubaa for Gaitech EDU
- * Year: 2016
- *
- */
-
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
@@ -20,10 +14,9 @@ int main(int argc, char **argv)
 	//create a node handle: it is reference assigned to a new node
 	ros::NodeHandle node;
 
-
     // Subscribe to a given topic, in this case "chatter".
 	//chatterCallback: is the name of the callback function that will be executed each time a message is received.
-    ros::Subscriber sub = node.subscribe("/turtle1/cmd_vel", 1000, chatterCallback);
+    ros::Subscriber sub = node.subscribe("chatter", 1000, chatterCallback);
 
     // Enter a loop, pumping callbacks
     ros::spin();
