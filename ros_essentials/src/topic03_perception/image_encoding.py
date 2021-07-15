1052b9c3f0c24e6039b27e1039bc6c2c760816cd
yufanana <yufan.fong@gmail.com>
2021-07-15 13:05:49 +0800

    Added image encoding

--- ros_essentials/src/topic03_perception/image_encoding.py
+++ ros_essentials/src/topic03_perception/image_encoding.py
@@ -7,7 +7,7 @@ import cv2
 image_name = "tree"
 
 print ('read an image from file')
-color_image = cv2.imread("images/tree.jpg",cv2.IMREAD_COLOR)
+color_image = cv2.imread("images/"+image_name+".jpg",cv2.IMREAD_COLOR)
 
 print ('display image in native color')
 cv2.imshow("Original Image",color_image)
@@ -16,36 +16,42 @@ print(color_image.shape)
 
 height,width,channels = color_image.shape
 
-print ('slipt the image into three channels.')
+# split image into three channels.
 blue,green,red = cv2.split(color_image)
 
-cv2.imshow("Blue Channel",blue)
-cv2.moveWindow("Blue Channel",0,height)
+# concatenate to combine 3 arrays into 1 image
+channel_imgs = np.concatenate((blue,green,red),axis=1)
+cv2.imshow("Blue, Green, Red Channel Image",channel_imgs)
 
-cv2.imshow("Red Channel",red)
-cv2.moveWindow("Red Channel",0,height)
+#cv2.imshow("Blue Channel",blue)
+#cv2.moveWindow("Blue Channel",250,0)
 
-cv2.imshow("Greeen Channel",green)
-cv2.moveWindow("Green Channel",0,height)
+#cv2.imshow("Red Channel",red)
+#cv2.moveWindow("Red Channel",500,0)
 
+#cv2.imshow("Greeen Channel",green)
+#cv2.moveWindow("Green Channel",750,0)
 
 # Hue: indicates the type of color that we see in a 360 degree format.
 # Saturation: an indication of how saturated an individual color is 
 # Value: indicates how luminous the channel is. 
 
-print ('---- slipt the image into Hue, Saturation, Value channels.----- ')
+# convert image to HSV
 hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 h,s,v = cv2.split(hsv)
 hsv_image = np.concatenate((h,s,v),axis=1)
 cv2.imshow("Hue, Saturation, Value Image",hsv_image)
 cv2.imshow("HSV Image",hsv)
+cv2.moveWindow("HSV Image",width*4,0)
 
 
-print ('------ converts an image to a grayscale ------')
+# convert image to grayscale
 gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray Image ",gray_image)
+cv2.moveWindow("Gray Image",width*5,0)
 
 print (gray_image)
 
+# close all windows conveniently
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- ros_essentials/src/topic03_perception/open_copy.py
+++ ros_essentials/src/topic03_perception/open_copy.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python
 
-#import numpy: the data structure that will handle an image
 import numpy as np
-
-#import openCV
 import cv2
- 
 
 image_name = "flower"
 
-print ('read an image from file')
+# read an image from file
 img = cv2.imread("images/"+image_name+".jpg")
 
-print ('create a window holder for the image')
-cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
+# create window holder for the image
+cv2.namedWindow("Image Viewer",cv2.WINDOW_NORMAL)
 
-print ('display the image')
-cv2.imshow("Image",img)
+# display the image
+cv2.imshow("Image Viewer",img)
 
+# wait for key press before proceeding
 print ('press a key inside the image to make a copy')
 cv2.waitKey(0)
 
-print ('image copied to folder images/copy/')
+# write image to folder images/copy/
 cv2.imwrite("images/copy/"+image_name+"-copy.jpg",img)
